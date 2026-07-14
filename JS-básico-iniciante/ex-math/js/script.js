6d293c216de4329d277a956342865e2d859d38a9
Amanda <amandaahaar@gmail.com>
2026-07-14 09:54:22 -0300

    alterações de pastas

--- JS-básico-iniciante/ex-math/js/script.js
+++ JS-básico-iniciante/ex-math/js/script.js
@@ -4,8 +4,10 @@ const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
 texto.innerHTML = `<p>Seu número - 2 é ${numero - 2}</p>`;
+texto.innerHTML += `<p>A raiz quadrada é: ${numero ** 0.5 }</p>`;
 texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
 texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`
 texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`
 texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`
 texto.innerHTML += `<p>Com duas casa decimais: ${Number(numero.toFixed(2))}<p>`
+
