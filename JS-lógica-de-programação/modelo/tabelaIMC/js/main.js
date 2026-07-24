71172c13987060312c2209a1640b266338bf08d3
Amanda <amandaahaar@gmail.com>
2026-07-24 15:28:09 -0300

    ajustes-posrelatorio

--- JS-lógica-de-programação/modelo/tabelaIMC/js/main.js
+++ JS-lógica-de-programação/modelo/tabelaIMC/js/main.js
@@ -19,6 +19,10 @@ function calcularImc() {
     const pesoNumero = Number(peso.value.replace(",", "."));
     const alturaNumero = Number(altura.value.replace(",", "."));
 
+    if (!pesoNumero || !alturaNumero || alturaNumero === 0) {
+      return NaN;
+    }
+
     const imc = pesoNumero / (alturaNumero * alturaNumero);
     return imc.toFixed(2); // "Quando alguém chamar a função, devolva o resultado do cálculo"
   }
@@ -27,23 +31,22 @@ function calcularImc() {
   botao.addEventListener("click", function (evento) {
     evento.preventDefault(); // página pára de recarregar sozinha
     const imc = calculoImc();
-    console.log(imc);
 
     resultado.classList.add("mostrar"); // Aparecer o resultado
 
     // Condições
     if (imc < 18.5) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Abaixo do peso)`;
-    } else if (imc >= 18.5 && imc <= 24.9) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Peso normal)`;
-    } else if (imc >= 25 && imc <= 29.9) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Sobrepeso)`;
-    } else if (imc >= 30 && imc <= 34.9) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 1)`;
-    } else if (imc >= 35 && imc <= 39.9) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 2)`;
-    } else if (imc >= 40 && imc < 190) {
-      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 3)`;
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Abaixo do peso)</p>`;
+    } else if (imc < 25) {
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Peso normal)</p>`;
+    } else if (imc < 30) {
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Sobrepeso)</p>`;
+    } else if (imc < 35) {
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 1)</p>`;
+    } else if (imc < 40) {
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 2)</p>`;
+    } else {
+      resultado.innerHTML = `<p>Seu IMC é: ${imc} (Obesidade grau 3)</p>`;
     }
   });
 }
